0a63dd98e899a3500af0daa150aa14cf6a5d32e3
Jung Woon Jik <dnswlrsla@gmail.com>
2026-04-28 13:41:02 +0900

    feat: 오디오 수신, 패킷 보정

---
+++ Audio/AudioPlayback.cs
@@ -0,0 +1,79 @@
+﻿using Concentus;
+using NAudio.Wave;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoiceChat.Audio
+{
+    public class AudioPlayback : IDisposable
+    {
+        private const int SAMPLE_RATE = 48000;
+        private const int CHANNELS = 1;
+        private const int FRAME_SAMPLES = SAMPLE_RATE * 20 / 1000;
+        private const int FRAME_BYTES = FRAME_SAMPLES * CHANNELS * 2;
+
+        private IOpusDecoder _decoder;
+        private BufferedWaveProvider _buffer;
+        private WaveOutEvent _waveOut;
+
+        public AudioPlayback()
+        {
+            // Opus decoder 초기화
+            _decoder = OpusCodecFactory.CreateDecoder(SAMPLE_RATE, CHANNELS);
+
+            // NAudio 재생 버퍼 초기화
+            _buffer = new BufferedWaveProvider(new WaveFormat(SAMPLE_RATE, 16, CHANNELS));
+            _buffer.BufferDuration = TimeSpan.FromSeconds(2); // 최대 2초 버퍼
+            _buffer.DiscardOnBufferOverflow = true; // 꽉차면 오래된거 버리기
+
+            // 스피커 출력 초기화
+            _waveOut = new WaveOutEvent();
+            _waveOut.Init(_buffer);
+        }
+
+        public void Start()
+        {
+            _waveOut.Play();
+        }
+
+        public void Stop()
+        {
+            _waveOut.Stop();
+        }
+
+        // JitterBuffer에서 호출 -> Opus 데이터 받아서 디코딩 후 재생
+        public void PlayOpus(byte[] opusData)
+        {
+            // null = 패킷 유실 = 무음 처리
+            if (opusData == null)
+            {
+                byte[] slience = new byte[FRAME_BYTES];
+                _buffer.AddSamples(slience, 0, FRAME_BYTES);
+                return;
+            }
+
+            // Opus -> PCM 디코딩
+            short[] pcm = new short[FRAME_SAMPLES];
+            int decodedSamples = _decoder.Decode(
+                opusData.AsSpan(),
+                pcm.AsSpan(0, FRAME_SAMPLES),
+                FRAME_SAMPLES
+            );
+
+
+            // short[] -> byte[] 변환, 재생 버퍼 추가
+            byte[] pcmBytes = new byte[decodedSamples * 2];
+            Buffer.BlockCopy(pcm, 0, pcmBytes, 0, pcmBytes.Length);
+            _buffer.AddSamples(pcmBytes, 0, pcmBytes.Length);
+        }
+
+        public void Dispose()
+        {
+            _waveOut?.Stop();
+            _waveOut?.Dispose();
+        }
+    }
+}
--- Forms/MainForm.cs
+++ Forms/MainForm.cs
@@ -3,13 +3,20 @@ using MaterialSkin.Controls;
 using System;
 using System.Collections.Generic;
 using System.Windows.Forms;
+using VoiceChat.Audio;
 using VoiceChat.Forms;
+using VoiceChat.protocol;
 
 namespace VoiceChat
 {
 
     public partial class MainForm : MaterialForm
     {
+        // Audio Test Code
+        private AudioCapture _audioCapture;
+        private AudioPlayback _audioPlayback;
+        private JitterBuffer _jitterBuffer;
+
         public MainForm()
         {
             InitializeComponent();
@@ -73,6 +80,51 @@ namespace VoiceChat
             };
 
             UpdateRoomList(dummyRooms);
+
+            // Audio Test Code
+            _audioPlayback = new AudioPlayback();
+            _jitterBuffer = new JitterBuffer();
+            _audioCapture = new AudioCapture();
+
+            _audioCapture.UserId = 1;
+            _audioCapture.RoomId = 100;
+
+            // JitterBuffer → AudioPlayback 콜백 연결
+            _jitterBuffer.OnPacketReady = (opusData) =>
+            {
+
+                if (opusData == null)
+                    Console.WriteLine("[AudioPlayback] 패킷 유실 → 무음 처리");
+                else
+                    Console.WriteLine($"[AudioPlayback] PlayOpus 호출 - opusData 크기: {opusData.Length} bytes");
+
+                _audioPlayback.PlayOpus(opusData);
+
+            };
+
+            // AudioCapture → JitterBuffer 콜백 연결
+            // @Todo UDPManager로 변경
+            // JitterBuffer 콜백 연결이 아니라 서버로 전송하는 로직으로 변경해야함
+            _audioCapture.OnPacketReady = (packet) =>
+            {
+                // 패킷에서 헤더 파싱
+                if (PacketHandler.DeserializeHeader(packet, packet.Length, out PacketHeader header))
+                {
+                    Console.WriteLine($"[AudioCapture] 패킷 생성 - 크기: {packet.Length} bytes / seq: {header.Sequence}");
+
+                    // payload 추출 (헤더 13바이트 이후)
+                    byte[] opusData = new byte[header.PayloadLength];
+                    Buffer.BlockCopy(packet, PacketConstants.HEADER_SIZE, opusData, 0, header.PayloadLength);
+
+
+                    Console.WriteLine($"[JitterBuffer] Push - seq: {header.Sequence} / 버퍼 크기: {header.PayloadLength} bytes");
+                    _jitterBuffer.Push(header, opusData);
+
+                }
+            };
+
+            _audioPlayback.Start();
+            _audioCapture.Start();
         }
     }
 
